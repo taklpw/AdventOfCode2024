ac98736d06690810d08be10592623ef0ba11c6bc
Kelly Lynch <k.lynch@acfr.usyd.edu.au>
2024-12-18 18:13:29 +1100

    Day 14 part 2 solution

--- AdventOfCode/Day14.cs
+++ AdventOfCode/Day14.cs
@@ -100,82 +100,57 @@ public class Day14 : BaseDay
         return new($"{quadrantValues[1] * quadrantValues[2] * quadrantValues[3] * quadrantValues[4]}");
     }
 
-    private string StringifyListofListofInt(List<List<int>> input)
+    private string Stringify2DArray(int[,] input, int width, int height)
     {
         string outString = "";
-        foreach (var line in input)
+        for (int x = 0; x < width; x++)
         {
-            foreach(var ch in line)
+            for (int y = 0; y < height; y++)
             {
-                outString += $"{ch}";
+                outString += $"{input[x, y]} ";
             }
             outString += "\n";
         }
         return outString;
     }
 
-    private long GetCompressedSize(string text)
-    {
-        byte[] buffer = Encoding.ASCII.GetBytes(text);
-        MemoryStream ms = new MemoryStream();
-        using (GZipStream zip = new GZipStream(ms, CompressionMode.Compress, true))
-        {
-            zip.Write(buffer, 0, buffer.Length);
-        }
-        return ms.Length;
-        //ms.Position = 0;
-        //MemoryStream outStream = new MemoryStream();
-
-        //byte[] compressed = new byte[ms.Length];
-        //ms.Read(compressed, 0, compressed.Length);
-
-        //byte[] gzBuffer = new byte[compressed.Length + 4];
-        //System.Buffer.BlockCopy(compressed, 0, gzBuffer, 4, compressed.Length);
-        //System.Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gzBuffer, 0, 4);
-        //return Convert.ToBase64String(gzBuffer);
-    }
-
     public override ValueTask<string> Solve_2()
     {
         const int width = 101;
         const int height = 103;
-        const int maxTime = 100000;
-        long minSize = -1;
+        int minSafetyFactor = -1;
 
-        //for (int i = 0; i < maxTime; i++)
         int i = 1;
         while(true)
         {
-            bool skipThisOne = false;
-            List<List<int>> outGrid = Enumerable.Repeat(Enumerable.Repeat(0, width).ToList(), height).ToList();
+            Dictionary<int, int> quadrantValues = new Dictionary<int, int>()
+            {
+                { -1, 0 },
+                { 0, 0 },
+                { 1, 0 },
+                { 2, 0 },
+                { 3, 0 },
+                { 4, 0 },
+            };
+            int[,] outGrid = new int[width, height];
+
             foreach (var state in _initialStates)
             {
-                (int x, int y) newPoint = (SaneModulo(state.pos.x + state.vel.x * i, width), SaneModulo(state.pos.y + state.vel.y * i, height));
-                outGrid[newPoint.y][newPoint.x]++;
-                //if (outGrid[newPoint.y][newPoint.x] > 1)
-                //{
-                //    skipThisOne = true;
-                //    break;
-                //}
+                (int x, int y) newPoint = (SaneModulo(state.pos.x + i * state.vel.x, width), SaneModulo(state.pos.y + i * state.vel.y, height));
+                quadrantValues[GetQuadrant(newPoint, width, height)]++;
+                outGrid[ newPoint.x, newPoint.y]++;
             }
-            string grid = StringifyListofListofInt(outGrid);
-            long size = GetCompressedSize(grid);
 
-            if (minSize == -1 || size < minSize)
+            int safetyFactor = quadrantValues[1] * quadrantValues[2] * quadrantValues[3] * quadrantValues[4];
+
+            if (minSafetyFactor == -1 || safetyFactor < minSafetyFactor)
             {
-                minSize = size;
-                Console.WriteLine($"New Minsize! At {i} Seconds, Size: {minSize}");
+                minSafetyFactor = safetyFactor;
+                Console.WriteLine($"New MinSafetyFactor! At {i} Seconds, Safety Factor: {safetyFactor}");
+                string grid = Stringify2DArray( outGrid, width, height );
+                Console.WriteLine( grid );
             }
-
-            //if (!skipThisOne)
-            //{
-            //    string stateString = StringifyListofListofInt(outGrid);
-            //    System.IO.Directory.CreateDirectory("./day14_files");
-            //    File.WriteAllText($"./day14_files/{i}.txt", stateString);
-            //    return new($"{i}");
-            //}
             i++;
         }
-        return new($"{1}");
     }
 }
